5ca843bc6c54396f6407eabcb48e539b17fa4e33
vcspartan <116987642+vcspartan@users.noreply.github.com>
2022-10-30 09:46:49 +0530

    Create Fab number.py

---
+++ Fab number.py
@@ -0,0 +1,8 @@
+#The math.fabs() method returns the absolute value of a number, as a float.
+
+#Import math Library
+import math
+
+#Print absolute values from numbers
+print(math.fabs(-66.43))
+print(math.fabs(-7))
